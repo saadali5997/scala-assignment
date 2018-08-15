1c08e6d12ed1774029c144f17b5e2583315e2c61
saadali5997 <saadali5997@gmail.com>
2018-08-15 11:37:21 +0500

    README.md added. Task2.scala formatted.

--- Task2.scala
+++ Task2.scala
@@ -1,17 +1,18 @@
 import scala.annotation.tailrec
 
 object Task2 {
-    
-    @tailrec
-    def factorial(number: BigInt, fact : BigInt = 1) : BigInt = 
+
+  @tailrec
+  def factorial(number: BigInt, fact: BigInt = 1): BigInt =
     {
-      number match {
-   	case 0 => fact
-   	case _ => factorial(number - 1, fact * number)
-}
+      if (number == 0)
+        return fact
+      else
+        factorial(number - 1, fact * number)
     }
-    def main(args: Array[String]) {
-      val number : BigInt = 123243
-      println(factorial(number))
-    }
+  def main(args: Array[String]) {
+    val number: BigInt = 12
+    //val fctrl : BigInt =
+    println(factorial(number))
+  }
 }
