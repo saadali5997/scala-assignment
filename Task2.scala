81d24a9072ab8bbdc945f84314483c510e92aaec
saadali5997 <saadali5997@gmail.com>
2018-08-15 11:18:52 +0500

    Made the proposed changes.

--- Task2.scala
+++ Task2.scala
@@ -3,14 +3,15 @@ import scala.annotation.tailrec
 object Task2 {
     
     @tailrec
-    def factorial(number: Int, fact : Int = 1) : Int = 
+    def factorial(number: BigInt, fact : BigInt = 1) : BigInt = 
     {
-      if(number == 0)
-        return fact
-      else
-        factorial(number - 1, fact*number)
+      number match {
+   	case 0 => fact
+   	case _ => factorial(number - 1, fact * number)
+}
     }
     def main(args: Array[String]) {
-      println(factorial(5))
+      val number : BigInt = 123243
+      println(factorial(number))
     }
 }
--- src/main/scala/com/datumbrain/webfetcher/MainClass.scala
+++ src/main/scala/com/datumbrain/webfetcher/MainClass.scala
@@ -5,29 +5,28 @@ import java.io._
 import System._
 import java.io.File
 
-
 object MainClass {
-  
-  def main(args : Array[String]) {
-    
+
+  def main(args: Array[String]) {
+
     // get response from url
     var response = Jsoup.parse(args(0))
-    
     // create folder with name = url(cleaned)
     // get time stamp
-    val timeStamp: Long = System.currentTimeMillis / 1000
+    val timeStamp: String = (System.currentTimeMillis / 1000).toString()
+    if (args.length > 0) {
+    val urlCleaned = args(0).replaceAll("[^A-Za-z0-9]", "")
     // generate folderName url(cleaned)+timeStamp
-    var folderName = args(0).replaceAll("[^A-Za-z0-9]","")+timeStamp.toString()
-
+    var folderName = f"$urlCleaned%s $timeStamp%s"
     // the folders doesn't exist initially
-    var dir :File = new File(folderName);
-    
+    var dir: File = new File(folderName);
     // create folder
     val successful: Boolean = dir.mkdirs()
-    
     // writting file output.html
-    val pw = new PrintWriter(folderName+"/output.html")
+    val pw = new PrintWriter(folderName + "/output.html")
     pw.write(response.toString())
     pw.close
+    }
+    else println("URL is required");    
   }
 }
